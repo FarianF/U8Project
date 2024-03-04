1ca44a4d8a11a739d37b9b1fe7ff556d133c865a
unknown <BT_4N2_18@BKTECH.LOCAL>
2024-03-04 13:30:40 -0500

    Completed part one

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] maze = getMaze("src/maze1");
+        for(int row = 0; row < maze.length; row++){
+            for(int col = 0; col<maze[0].length; col++){
+                String dot = maze[row][col];
+                String adjacent = 
+                if(dot.equals(".")){
+
+                }
+            }
+        }
+
+
+    }
+
+    public static String[][] getMaze(String fileName) {
+        File f = new File(fileName);
+        Scanner s = null;
+        try {
+            s = new Scanner(f);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            System.exit(1);
+        }
+
+        ArrayList<String> fileData = new ArrayList<String>();
+        while (s.hasNextLine())
+            fileData.add(s.nextLine());
+
+        int rows = fileData.size();
+        int cols = fileData.get(0).length();
+
+        String[][] maze = new String[rows][cols];
+
+        for (int i = 0; i < fileData.size(); i++) {
+            String d = fileData.get(i);
+            for (int j = 0; j < d.length(); j++) {
+                maze[i][j] = d.charAt(j) + "";
+            }
+        }
+        return maze;
+
+    }
+
+}
