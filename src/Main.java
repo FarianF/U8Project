be855a8233adab8f7131be41c45ac9a466896d49
BT_4N2_18 <fsfr@fdsfsd>
2024-03-05 13:56:30 -0500

    Stilll working on part 1

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String[][] maze = getMaze("src/maze1");
-        for(int row = 0; row < maze.length; row++){
-            for(int col = 0; col<maze[0].length; col++){
-                String dot = maze[row][col];
-                String adjacent = 
-                if(dot.equals(".")){
 
+        for(int row = 0; row < maze.length; row++){
+            for(int col = 0; col < maze[0].length; col++){
+                String dot = maze[row][col];
+                if(dot.equals(".")){
+                    System.out.println(row + "," + col);
                 }
             }
         }
